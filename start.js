3e7b1cb66536c08563198662481c1bafca733047
Kirupakaran <kirupakaranh@gmail.com>
2023-04-19 17:14:58 +0530

    Make node talk to postgres

--- start.js
+++ start.js
@@ -1,17 +1,33 @@
 const nano = require('nano')('https://replicate.npmjs.com')
+const { Sequelize } = require('sequelize');
+
+const postgresConn = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_URL}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`
+const sequelize = new Sequelize(postgresConn)
 const db = nano.db.use('registry')
 
-let lastSequence = 0
+function testConnection() {
+  try {
+    sequelize.authenticate()
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
-db.changesReader.start({
-  includeDocs: false,
-  since: lastSequence,
-  batchSize: 10
-})
-  .on('batch', (b) => {
-    console.log('batch', b)
-  }).on('seq', (s) => {
-    lastSequence = s
-  }).on('error', (e) => {
-    console.error('error', e);
+function start() {
+  let lastSequence = 0
+
+  db.changesReader.start({
+    includeDocs: false,
+    since: lastSequence,
+    batchSize: 10
   })
+    .on('batch', (b) => {
+      console.log('batch', b)
+    }).on('seq', (s) => {
+      lastSequence = s
+    }).on('error', (e) => {
+      console.error('error', e);
+    })
+}
+testConnection().then(start)
